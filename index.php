59a7c75a792d71ed7edc6ac528f238c30ad5c79f
dejavis <misdatosdeseguridadkey@gmail.com>
2023-03-06 22:41:23 -0600

    Se creo menu para crear tabla e insertar datos en oracle

--- index.php
+++ index.php
@@ -5,7 +5,15 @@
 </head>
 <body>
 <h1>ORACLE Y PHP</h1>
-<form>
+<form method="POST" action="index.php">
+	<select name="opciones">
+		<option value="INSERTAR">INSERTAR</option>
+		<option value="ELIMINAR">ELIMINAR</option>
+		<option value="ACTUALIZAR">ACTUALIZAR</option>
+		<option value="CREAR">CREAR</option>
+	</select><br>
+	<h3>Nombre: </h3><input type="text" name="nombre" required="INTRODUCIR DATO">
+	<h3>Edad: </h3><input type="number" name="edad">
 	<input type="submit" value="Ejecutar Click" name="ejecutor">
 </form>
 <?php
@@ -21,15 +29,46 @@ echo "TABLA CREADA EXITOSAMENTE";
 
     if($c = OCILogon("SYSTEM", "oraclebase21", $db))
     {
-    	$sql = "CREATE TABLE amigos(nombre varchar2(15), edad number(2))";
+    	if(isset($_POST['ejecutor'])){
+    		$opciones=$_POST['opciones'];
+    		$nombre=$_POST['nombre'];
+    		$edad=$_POST['edad'];
+
+    		switch ($opciones) {
+    			case 'INSERTAR':
+    				$sql = "INSERT INTO amigos VALUES('$nombre','$edad')";
+					$unir = oci_parse($c, $sql);
+					oci_execute($unir);
+					echo "DATOS INSERTADOS EXITOSAMENTE";
+        			echo "Successfully connected to Oracle.\n";
+    				break;
+    			case 'ELIMINAR':
+    				# code...
+    				break;
+    			case 'ACTUALIZAR':
+    				# code...
+    				break;
+    			case 'CREAR':
+    				$sql = "CREATE TABLE amigos(nombre varchar2(15), edad number(2))";
+					$unir = oci_parse($c, $sql);
+					oci_execute($unir);
+					echo "TABLA CREADA EXITOSAMENTE";
+        			echo "Successfully connected to Oracle.\n";
+    				break;
+    			
+    			default:
+    				# code...
+    				break;
+    		}
+    	}
+    	/*$sql = "CREATE TABLE amigos(nombre varchar2(15), edad number(2))";
 		$unir = oci_parse($c, $sql);
 		oci_execute($unir);
 		echo "TABLA CREADA EXITOSAMENTE";
         echo "Successfully connected to Oracle.\n";
+        */
         OCILogoff($c);
-    }
-    else
-    {
+    }else{
         $err = OCIError();
         echo "Connection failed." . $err[text];
     }
